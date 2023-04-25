1e98538c0cbe141e94b007ddece6579d905a4043
MaxAndFelix <2415413585@qq.com>
2023-04-26 00:39:43 +0800

    init

---
+++ SJTU-RM-CV-2019/build/CMakeFiles/create-startup.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create-startup"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/create-startup.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SJTU-RM-CV-2019/build/CMakeFiles/train-cnn.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train-cnn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/train-cnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
